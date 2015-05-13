565317ac754b165e7b24cb91f92764b6333d6bb5
femartinez92 <femartinez@uc.cl>
2015-05-13 00:36:28 -0400

    autenticacion de token funcionando

--- app/controllers/api/v1/b2b_controller.rb
+++ app/controllers/api/v1/b2b_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::B2bController < ApplicationController
 	protect_from_forgery with: :null_session
-	before_action :authorize, except:[:create_group, :documentation, :get_token]
+	before_filter :authorize, except:[:create_group, :documentation, :get_token]
 	before_action :group_params, only: [:create_group, :get_token]
 	before_action :order_params, only: [:create_order, :accepted_order, :rejected_order, :canceled_order]
 
@@ -49,38 +49,56 @@ class Api::V1::B2bController < ApplicationController
 	end
 
 	# Métodos orden de compra
+	# Antes de todos estos métodos se valida el token y se setea la variable @order con el id
 
 	#Este método avisa que se generó una orden de compra
 	def create_order
-		
+		respond_to do |format|
+			format.json {render json: {},status:200}
+		end
 	end
 	#Este método avisa que una orden de compra fue aceptada
 	def accepted_order
-		
+		respond_to do |format|
+			format.json {render json: {},status:200}
+		end
 	end
 
 	#Este método avisa que una orden de compra fue rechazada
 	def rejected_order
-
+		respond_to do |format|
+			format.json {render json: {},status:200}
+		end
 	end
 
 	#este método avisa que una orden de compra fue cancelada
 	def canceled_order
-
+		respond_to do |format|
+			format.json {render json: {},status:200}
+		end
 	end
 
 	# Métodos facturas
+	# Antes de todos los métodos de facturas (invoice) se setea la variable @invoice con el
+	# id de la factura entregado en el request y se verifica el token
+
 	# Este método avisa que se genero una factura
 	def issued_invoice
-		
+		respond_to do |format|
+			format.json {render json: {},status:200}
+		end
 	end
 	# Este método avisa que una factura se pago
 	def invoice_paid
-		
+		respond_to do |format|
+			format.json {render json: {},status:200}
+		end
 	end
 	# Este método avisa que una factura fue rechazada
 	def rejected_invoice
-		
+		respond_to do |format|
+			format.json {render json: {},status:200}
+		end
 	end
 
 	#Este metodo es para la página estática de la documentación
@@ -112,10 +130,11 @@ class Api::V1::B2bController < ApplicationController
 
 	#Este metodo ve si el token provisto es valido, aún no esta probado
 	def authorize
-		token
-		if ApiKey.where(access_token: @token)
+		token #Seteamos la variable @token
+		if ApiKey.where(access_token: @token).first
 			return true
 		else
+			p 'token invalido'
 			respond_to do |format|
 				format.json {render json: {description: 'Invalid token'},status: :unauthorized}
 			end
